55ee96f83186268facb0dd50762ec90878080c71
Nick Conklin <nconkl1@outlook.com>
2018-01-30 23:46:59 -0500

    Formatting

--- src/com/conklin/Main.java
+++ src/com/conklin/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // Print formatter for headings
         System.out.printf("%-40s%s\t\t%s\n%-40s%s\t\t%s", "Deposit Threads", "Withdraw Threads", "Balance",
                 "----------------", "----------------", "-------");
+
         Thread depositThread1 = new Thread(new DepositThread("D1")); // Create thread D1
         Thread depositThread2 = new Thread(new DepositThread("D2")); // Create thread D2
         Thread depositThread3 = new Thread(new DepositThread("D3")); // Create thread D3
